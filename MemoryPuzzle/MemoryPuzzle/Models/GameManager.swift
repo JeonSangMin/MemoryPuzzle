8c79d9af5fad60508f1ef1479a3bc74c1c64e474
전상민 <jeonsangmin77@gmail.com>
2020-05-12 14:21:41 +0900

    [Fix] GameManager advanced more functionality

--- MemoryPuzzle/MemoryPuzzle/Models/GameManager.swift
+++ MemoryPuzzle/MemoryPuzzle/Models/GameManager.swift
@@ -11,8 +11,12 @@ import AVFoundation
 import UIKit
 
 final class GameManager {
-    var audioPlayer = AVAudioPlayer()
+    private var audioPlayer = AVAudioPlayer()
+    private var timer = Timer()
+    private var isPause = false
+    private var time: Double = 0
     
+    // 문열리기
     fileprivate func openTheGate(leftDoor: UIImageView, rightDoor: UIImageView) {
         UIView.animate(withDuration: 1, animations: {
             leftDoor.transform = CGAffineTransform(translationX: -(UIScreen.main.bounds.width / 2), y: 0)
@@ -25,6 +29,7 @@ final class GameManager {
         }
     }
     
+    // 카드 뒤집는 소리
     fileprivate func flipCardAudio() {
         guard let soundAsset: NSDataAsset = NSDataAsset(name: "flip1") else {
             print(#function, " failed to play")
@@ -38,6 +43,7 @@ final class GameManager {
         }
     }
     
+    // 문 닫히는 소리
     fileprivate func closeTheGateAudio() {
         guard let soundAsset: NSDataAsset = NSDataAsset(name: "jailDoor") else {
             print(#function, " failed to play")
@@ -49,5 +55,32 @@ final class GameManager {
         } catch let error as NSError {
             print("Error : \(error.code), Message : \(error.localizedDescription)")
         }
+    }
+    
+    
+    // MARK: TimeManaging
+    func startTimer(countDownLabel: UILabel) {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        countDownLabel.text = String(format: "%.2f", time)
+    }
+    
+    @objc func update() {
+        if(time >= 0.00) {
+            time += 0.01
+//            countDownLabel.text = String(format: "%.2f", time)
+        }
+    }
+    
+    @objc func pause(_ sender: UIButton, countDownLabel: UILabel) {
+        if isPause {
+            startTimer(countDownLabel: countDownLabel)
+//            pauseLabel.isHidden = true
+//            view.sendSubviewToBack(pauseLabel)
+        } else {
+            timer.invalidate()
+//            pauseLabel.isHidden = false
+//            view.bringSubviewToFront(self.pauseLabel)
+        }
+        isPause.toggle()
     }
 }
