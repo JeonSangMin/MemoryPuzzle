792ddb0733f6a7029bd3c87b2b32949020936623
전상민 <jeonsangmin77@gmail.com>
2020-05-21 18:39:26 +0900

    [Fix] modified property name

---
+++ MemoryPuzzle/MemoryPuzzle/Controllers/RecordsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecordsViewController.swift
+//  MemoryPuzzle
+//
+//  Created by MyMac on 2020/05/21.
+//  Copyright © 2020 sandMan. All rights reserved.
+//
+
+import UIKit
+
+class RecordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MemoryPuzzle/MemoryPuzzle/Views/RecordsView.swift
@@ -0,0 +1,21 @@
+//
+//  RecordsView.swift
+//  MemoryPuzzle
+//
+//  Created by MyMac on 2020/05/21.
+//  Copyright © 2020 sandMan. All rights reserved.
+//
+
+import UIKit
+
+class RecordsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
